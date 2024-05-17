745ff4b24b67688095858893812116b4e71b631d
Manu Cuberos <mcuberos@hispacold.es>
2024-05-17 14:58:31 +0200

    Nuevo Fichero CreaFicheroLegible

--- CreaFicheroLegible.py
+++ CreaFicheroLegible.py
@@ -1,7 +1,3 @@
-from tkinter import messagebox
-import getpass
-import os
-
 action=input("Seleccione una opción (inserte solo el número): \n1. Cargar un pedido de CAF para generar Excel legible. \n2. Comparar dos versiones de un pedido de CAF \n3. Cargar un CBC para generar un excel temporal con la propuesta de autorrellenado. \n4. Cargar un excel temporal para autorellenar un CbC \n")
 '''
 action=input("Seleccione una opción (inserte solo el número): \n1. Crear una Base de Datos (SOLO ADMIN). \n2. Cargar un CBC a la Base de Datos (SOLO ADMIN). \n3. Cargar un fichero temporal a la base de datos (SOLO ADMIN). \n4. Cargar un CBC para generar un excel temporal con la propuesta de autorrellenado. \n5. Cargar un excel temporal para autorellenar un CbC \n")
@@ -12,28 +8,3 @@ if action=="1":
     else:
         messagebox.showinfo("Acceso Denegado","La contraseña introducida es incorrecta.")
 '''
-
-if action=="1":
-    pwd=getpass.getpass("INTRODUZCA LA CONTRASEÑA: ")
-    if pwd=="MCG@Trenes":
-        import LoadDatabase
-    else:
-        messagebox.showinfo("Acceso Denegado","La contraseña introducida es incorrecta.")
-
-elif action=="2":
-    pwd=getpass.getpass("INTRODUZCA LA CONTRASEÑA: ")
-    if pwd=="MCG@Trenes":
-        import LoadDatabaseFromTemp
-    else:
-        messagebox.showinfo("Acceso Denegado","La contraseña introducida es incorrecta.")
-
-elif action=="3":
-    import CreateCBCTemp
-
-elif action=="4":
-    import AutoFillFromCBCTemp
-
-else:
-    messagebox.showinfo("WARNING!","AN INVALID OPTION HAS BEEN SELECTED. PLEASE RUN THE PROGRAM AGAIN.")
-
-input("PRESIONE ENTER PARA SALIR")
